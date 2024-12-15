7b59a45660411ec4362b66c49b3d155d5c5b8c5d
BuciMr <51615775+baninations@users.noreply.github.com>
2024-12-15 14:54:10 +0100

    Style the active link in the navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,13 +1,38 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { CgProfile } from "react-icons/cg";
 import { GiVacuumCleaner } from "react-icons/gi";
 
 export default function NavBar() {
+  const currentPath = usePathname();
+  const links = [
+    { href: "/", label: "Dashboard" },
+    { href: "/work", label: "Work" },
+  ];
+
   return (
     <nav className="flex justify-between space-x-6 px-16 h-16 border-b items-center">
       <Link href="/">
         <GiVacuumCleaner size={28} />
       </Link>
-      <div>Profile</div>
+      <ul className="flex space-x-6 ">
+        {links.map((link) => (
+          <li
+            className={
+              link.href === currentPath
+                ? "text-zinc-900"
+                : `text-zinc-500 hover:text-zinc-800 transition-colors`
+            }
+          >
+            <Link href={link.href}>{link.label}</Link>
+          </li>
+        ))}
+      </ul>
+      <Link href="/">
+        <CgProfile size={28} />
+      </Link>
     </nav>
   );
 }
