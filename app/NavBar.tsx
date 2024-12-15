8213d05b9de72444e4bbe8f9c63ea11501fffe14
BuciMr <51615775+baninations@users.noreply.github.com>
2024-12-15 11:33:35 +0100

    Build the navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,9 +1,12 @@
 import Link from "next/link";
+import { GiVacuumCleaner } from "react-icons/gi";
 
 export default function NavBar() {
   return (
     <nav className="flex justify-between space-x-6 px-16 h-16 border-b items-center">
-      <Link href="/">Logo</Link>
+      <Link href="/">
+        <GiVacuumCleaner size={28} />
+      </Link>
       <div>Profile</div>
     </nav>
   );
